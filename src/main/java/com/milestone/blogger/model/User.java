dc377cab2e2a0a03a074ecdb59e141aba6935824
MaxwellsDaemon-ca <thejollycanadian@yahoo.com>
2024-12-15 19:03:43 -0500

    init

---
+++ src/main/java/com/milestone/blogger/model/User.java
@@ -0,0 +1,13 @@
+package com.milestone.blogger.model;
+
+import java.util.Date;
+
+public class User {
+    
+    private String username;
+    private String email;
+    private String passwordHash;
+    private String dobString;
+    private Date datejoined;
+
+}
